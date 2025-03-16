796b22e8d6dc5dd682abf00c40871fc7fe9bd98c
Artiom Petrachi <artiom.petrachi@asperitas-solutions.com>
2025-03-16 17:05:10 +0200

    0203-direct-change

--- frontend/src/pages/bookables/BookablesList.tsx
+++ frontend/src/pages/bookables/BookablesList.tsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { bookables } from "../../static.json";
 import { Button } from "react-bootstrap";
 
 export function BookablesList() {
   const group = "Rooms";
   const bookablesInGroup = bookables.filter((b) => b.group === group);
-  const [bookableIndex, setBookableIndex] = useState(1);
-  console.log(bookableIndex);
+  // const [bookableIndex, setBookableIndex] = useState(1);
+
+  let bookableIndex = 1;
+
+  function changeBookable(selectedIndex: number) {
+    bookableIndex = selectedIndex;
+    console.log(bookableIndex);
+  }
 
   return (
     <ul className="bookables items-list-nav">
@@ -16,7 +22,7 @@ export function BookablesList() {
             key={b.id}
             className={i === bookableIndex ? "selected" : undefined}
           >
-            <Button className="p-2" onClick={() => setBookableIndex(i)}>
+            <Button className="p-2" onClick={() => changeBookable(i)}>
               {b.title}
             </Button>
           </li>
