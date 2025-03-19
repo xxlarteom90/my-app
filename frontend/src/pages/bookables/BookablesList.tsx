dcba6b92fc0ecc0538baa213c81808c3963c165d
Artiom Petrachi <artiom.petrachi@asperitas-solutions.com>
2025-03-19 19:59:28 +0200

    0206-next-button

--- frontend/src/pages/bookables/BookablesList.tsx
+++ frontend/src/pages/bookables/BookablesList.tsx
@@ -1,27 +1,42 @@
 import { useState } from "react";
 import { bookables } from "../../static.json";
 import { Button } from "react-bootstrap";
+import { FaArrowRight } from "react-icons/fa";
 
 export function BookablesList() {
   const group = "Rooms";
   const bookablesInGroup = bookables.filter((b) => b.group === group);
   const [bookableIndex, setBookableIndex] = useState(1);
 
+  function nextBookable() {
+    setBookableIndex((i) => (i + 1) % bookablesInGroup.length);
+  }
+
+  console.log(bookableIndex);
 
   return (
-    <ul className="bookables items-list-nav">
-      {bookablesInGroup.map((b, i) => (
-        <span className="p-2">
-          <li
-            key={b.id}
-            className={i === bookableIndex ? "selected" : undefined}
-          >
-            <Button className="p-2" onClick={() => setBookableIndex(i)}>
-              {b.title}
-            </Button>
-          </li>
-        </span>
-      ))}
-    </ul>
+    <div>
+      <ul className="bookables items-list-nav">
+        {bookablesInGroup.map((b, i) => (
+          <span className="p-2">
+            <li
+              key={b.id}
+              className={i === bookableIndex ? "selected" : undefined}
+            >
+              <Button className="p-2" onClick={() => setBookableIndex(i)}>
+                {b.title}
+              </Button>
+            </li>
+          </span>
+        ))}
+      </ul>
+
+      <p>
+        <Button onClick={nextBookable} autoFocus>
+        <FaArrowRight/>
+          Next
+        </Button>
+      </p>
+    </div>
   );
 }
