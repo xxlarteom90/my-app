e9643f2ec97c8d516bbb61cf7ab9e64a92a92fad
Artiom Petrachi <artiom.petrachi@asperitas-solutions.com>
2025-03-26 00:42:21 +0200

    simple-reducer-counter

--- frontend/src/pages/booking/Bookings.tsx
+++ frontend/src/pages/booking/Bookings.tsx
@@ -1,10 +1,56 @@
-import { Link } from "react-router-dom";
+// import { Button } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+
+type Action =
+  | { type: "add"; payload: number }
+  | { type: "sub"; payload: number }
+  | { type: "set"; payload: number }
+  | { type: "inc" }
+  | { type: "dec" };
 
 export function Bookings() {
+  let count = 0;
+
+  function reducer(state: number, action: Action) {
+    // if (action.type === "inc") {
+    //   return state + 1;
+    // }
+    // if (action.type === "dec") {
+    //   return state - 1;
+    // }
+    if (action.type === "add") {
+      return state + action.payload;
+    }
+    if (action.type === "sub") {
+      return state - action.payload;
+    }
+    if (action.type === "set") {
+      return action.payload;
+    }
+
+    return state;
+  }
+
+  count = reducer(count, {type: "add", payload: 3})
+        count = reducer(count, {type: "sub", payload: 10})
+        count = reducer(count, {type: "set", payload: 41})
+        count = reducer(count, {type: "set", payload: 44})
+        count = reducer(count, {type: "inc"})
+        count = reducer(count, {type: "dec"})
+        count = reducer(count, {type: "dec"})
+console.log(count)
+
   return (
     <header>
       <li>
-        <Link to="bookings"> Bookings</Link>
+        <span className="mx-4">
+          {count}
+        {/* {count = reducer(count, {type: "add", payload: 3})}
+        {count = reducer(count, {type: "sub", payload: 10})}
+        {count = reducer(count, {type: "set", payload: 41})}
+        {count = reducer(count, {type: "inc"})} */}
+        </span>
+        {/* <Link to="bookings"> Bookings</Link> */}
       </li>
     </header>
   );
