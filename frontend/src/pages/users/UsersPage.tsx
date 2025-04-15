c751c0f2c98e40d7bd692634449ff523cfd79394
Artiom Petrachi <artiom.petrachi@asperitas-solutions.com>
2025-04-15 21:57:54 +0300

    userStorage-c987h

--- frontend/src/pages/users/UsersPage.tsx
+++ frontend/src/pages/users/UsersPage.tsx
@@ -1,8 +1,10 @@
+import { UserStorage } from '../helloPage/UserStorage'
 import { UsersList } from './UsersList'
 
 export  function UsersPage() {
   return (
     <div>
+      <UserStorage/>
       <UsersList/>
     </div>
   )
